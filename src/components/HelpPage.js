67740fd252ceb557a69e6dab3df306aa343b33d5
oguzhoto <oguzhoto@gmail.com>
2021-08-28 14:30:42 +0300

    store user data in user area

--- src/components/HelpPage.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const HelpPage = () => <div>This is my add HelpPage component</div>;
-export default HelpPage;
---
+++ src/routers/PublicRoute.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+
+const PublicRoute = ({
+  isAuthanticated,
+  component: Component,
+  ...rest
+}) => (
+  <Route {...rest} component={(props)=> (
+    isAuthanticated ? ( 
+      <Redirect to="/dashboard" />
+    ) : (
+      <Component {...props}/>
+    )
+  )}/>
+  
+);
+
+const mapStateToProps = (state) => ({
+  isAuthanticated: !!state.auth.uid
+});
+
+export default connect(mapStateToProps)(PublicRoute);
